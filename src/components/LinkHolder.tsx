e605bf88233810b9665e81b75ae599fd4af18307
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-01-30 22:57:18 +0300

    feat:link shortening functionality

--- src/components/LinkHolder.tsx
+++ src/components/LinkHolder.tsx
@@ -1,6 +1,7 @@
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import "./styles/LinkHolder.css"
 import DeleteIcon from '@mui/icons-material/Delete';
+import useSWR from "swr";
 interface LinkHolderProps{
     shortLink: string,
     originalUrl :string,
@@ -8,15 +9,28 @@ interface LinkHolderProps{
     id:number
 }
 const LinkHolder =({shortLink,originalUrl,date,id}:LinkHolderProps)=>{
+    const DELETE_URL = import.meta.env.VITE_DELETE_URL
+    const deleteById =(async ()=>{
+        const response =await fetch(DELETE_URL+id,{
+            method:"DELETE"
+        })
+        if(response.ok){
+            const data =await response.json()
+            console.log(data.message)
+        }
+    })
+
     return(
-        <div className={"link__holder"}>
-            <div className={"link__holder_left"}>
+        <div className={"link__holder"} id={id}>
+            <div className={"link__holder__left"}>
                 <h3>{shortLink}</h3>
                 <p>{originalUrl}</p>
             </div>
             <div className={"link__holder_right"}>
-                <button><ContentCopyIcon/></button>
-                <button><DeleteIcon/></button>
+                <button ><ContentCopyIcon/></button>
+                <button onClick={()=>{
+                    deleteById()
+                }}><DeleteIcon/></button>
             </div>
             {/*<div>{date}</div>*/}
         </div>
--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,13 +1,51 @@
 import "./styles/Dashboard.css"
-import SettingsIcon from "@mui/icons-material/Settings";
 import LinkIcon from "@mui/icons-material/Link";
 import AssessmentIcon from '@mui/icons-material/Assessment';
 import LinkHolder from "../components/LinkHolder.tsx";
 import Header from "../components/Header.tsx";
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import {useState} from "react";
+import useSWR from "swr";
 
 const DashBoard =()=>{
+    const CREATE_URL =import.meta.env.VITE_CREATE_SHORTURL
+    const GET_ALL =import.meta.env.VITE_GET_ALL
+    const[longUrl,setLongUrl] =useState<string>("")
+    const[site,setSite] =useState<string>("")
+    const [iframeImage,setIframeImage] =useState("https://i.pinimg.com/236x/f8/bf/67/f8bf670258af7e018ebba54a5b446aa4.jpg")
+    const[shortURL,setShortUrl] =useState<string>("")
+    const[allUrls,setAll] =useState<string[]>([])
+
+    const myHeaders =new Headers();
+    myHeaders.append("Content-Type","application/json")
+
+    const createShortUrl =(async ()=>{
+        const response =await fetch(CREATE_URL,{
+            method:"POST",
+            body:JSON.stringify({
+                originalUrl: longUrl,
+                userId: "1",
+            }),
+            headers:myHeaders
+        })
+        if(response.ok){
+            const data =await response.json()
+            if(data.statusCode ==200){
+                setShortUrl("http://localhost:8082/api/v1/shrtn/redirect/"+data.urlModel.shortUrl)
+            }
+        }
+    })
+    const fetchAllUrls =(async ()=>{
+        const response =await fetch(GET_ALL)
+        if(response.ok){
+            const data =await response.json()
+            setAll(data.urlModels)
+        }
+    })
+    const {isLoading,error} =useSWR(GET_ALL,fetchAllUrls)
+
+
     return(
         <div className={"dashboard__container"}>
             <Header/>
@@ -35,42 +73,31 @@ const DashBoard =()=>{
                     </div>
                     <div className={"all_links"}>
                         <h2>ENGAGEMENT ALL TIME</h2>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
-                        <LinkHolder shortLink={"wertgfdrtegdf"} originalUrl={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} id={1} date={"2022:22"}/>
+                        {allUrls.map((urls)=>(
+                            <LinkHolder shortLink={urls.shortUrl} originalUrl={urls.originalUrl} id={urls.id} date={urls.createdOn}/>
+                        ))}
+
                     </div>
                 </div>
                 <div className={"create__new"}>
                     <h1>CREATE NEW LINK <LinkIcon/></h1>
                     <p>Create,short and manage your links</p>
-                    <form className={"url__input__form"}>
-                        <input type={"url"} className={"link__input"}/>
-                        <button className={"create__link__button"}>Create Link <ArrowRightAltIcon/></button>
+                    <form className={"url__input__form"} onSubmit={(e)=>{
+                        e.preventDefault()
+                        createShortUrl()
+                    }} >
+                        <input type={"url"} className={"link__input"} onChange={(e)=>{
+                            setLongUrl(e.target.value)
+                        }}/>
+                        <button className={"create__link__button"} type={"submit"}>Create Link <ArrowRightAltIcon/></button>
                     </form>
                     <div className={"view__iframe"}>
-                        <iframe src="https://i.pinimg.com/236x/10/ec/d3/10ecd389899f7f53bacdb67d091602b0.jpg" width="100%" height="280px" ></iframe>
+                        <iframe src={longUrl?longUrl:iframeImage} width="100%" height="280px" ></iframe>
+
+                        <p>shrtnd Url:</p>
+                        <div className={"show_formed__link"}>
+                            <h3><LinkIcon/> {shortURL}</h3>
+                        </div>
                     </div>
                     <div className={"qrcode__gen"}>
                         <div className={"download_qr"}>
@@ -78,7 +105,7 @@ const DashBoard =()=>{
                             <button className={"download__qr__button"}>Download PNG</button>
                         </div>
                         <div className={"actual_qr_image"}>
-                            <img src={"https://i.pinimg.com/736x/01/48/1d/01481d0e8aaaf684a6f61efb75923dcd.jpg"}className={"qr_code__image"} alt={"qr code"}/>
+                            <img src={"https://i.pinimg.com/736x/01/48/1d/01481d0e8aaaf684a6f61efb75923dcd.jpg"} className={"qr_code__image"} alt={"qr code"}/>
                             <div className={"qr__links"}>
                                 <div>
                                     <h4> <span><LinkIcon/></span>link</h4>
@@ -99,3 +126,5 @@ const DashBoard =()=>{
     )
 }
 export default DashBoard
+
+
