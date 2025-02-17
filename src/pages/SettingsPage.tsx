78bdc07e67c1bbbe0f6894e8901dd91b00275632
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-02-17 17:52:58 +0300

    feat: added profile images

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -8,16 +8,38 @@ interface UserProps{
 const SettingsPage =()=>{
     const USER_ID =1
     const GET_USER =`http://localhost:8080/api/v1/users/${USER_ID}/get`
+    const UPLOAD_PROFILEPHOTO_URL =`http://localhost:8080/api/v1/users/${USER_ID}/upload-profile`
     const[userDetails,setUserDetails] =useState<UserProps>({email: "", username: ""})
+    const[imageSrc,setImageSrc]=useState("")
+    const[newImage,setNewImage] =useState("")
+    const[editWindow,setEditWindow] =useState(false)
 
     const fetchUserById=(async ()=>{
         const response =await fetch(GET_USER)
         const data =await response.json()
         setUserDetails(data?.user)
+
     })
     useEffect(()=>{
        fetchUserById()
+        fetchProfilePhoto()
     },[])
+    const fetchProfilePhoto =(async ()=>{
+        const response =await fetch("http://localhost:8080/api/v1/users/fetch-profile?userId=1")
+        const data =await response.blob()
+        const myObject =URL.createObjectURL(data)
+        setImageSrc(myObject)
+    })
+    const changeProfilePhoto =(async ()=>{
+        const formData = new FormData();
+        formData.append("profilePhoto",newImage)
+        const response =await fetch(UPLOAD_PROFILEPHOTO_URL,{
+            method:"PUT",
+            body:formData
+        })
+        const data =await response.json()
+        console.log(data)
+    })
 
     return(
         <div className={"settings_container"}>
@@ -30,7 +52,7 @@ const SettingsPage =()=>{
                 <h4>Basics</h4>
                 <div className={"profile__photo__container"}>
                     <h4>Photo</h4>
-                    <img src={"https://i.pinimg.com/236x/31/ca/cf/31cacfc8bceb2011c2f23ea32d2fbfa1.jpg"} alt={"profile"}/>
+                    <img src={imageSrc} alt={"profile"}/>
                     <button>Edit</button>
                 </div>
                 <div className={"profile__photo__container"}>
@@ -41,9 +63,25 @@ const SettingsPage =()=>{
                 <div className={"profile__photo__container"}>
                     <h4>Email address</h4>
                     <p>{userDetails.email}</p>
-                    <button>Edit</button>
+                    <button onClick={()=>{
+                        setEditWindow(!editWindow)
+                    }}>Edit</button>
                 </div>
             </div>
+            {editWindow&&
+                <div className={"edit__window"}>
+                    <form onSubmit={(e)=>{
+                        e.preventDefault()
+                        changeProfilePhoto()
+                    }}>
+                        <h2>Change profile photo</h2>
+                        <input type={"file"} onChange={(e)=>{
+                            setNewImage(e.target.files[0])
+                        }}/>
+                        <button type={"submit"}>update</button>
+                    </form>
+                </div>
+            }
 
         </div>
     )
